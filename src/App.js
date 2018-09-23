fce9bbaa2e5aa2dc4714e30a86143860d331cda1
Jordan <tjordanwells@gmail.com>
2018-09-23 14:56:35 -0400

    yarn build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Game from './components/Game'
 
-const App = () => (
-  <Router>
-    <Route exact path="/" component={Game} />
-  </Router>
-);
+const App = () => <Game />;
 
 export default App;
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import logo from '../images/Rick_and_Morty_logo.png';
 const Nav = props => (
   
   <nav className="navbar navbar-dark bg-dark sticky-top">
-    <a className="navbar-brand ml-4" href="#">
+    <a className="navbar-brand ml-4" href="/">
       <img src={logo} height="100px" width="280px" className="d-inline-block align-top" alt="" />
     </a>
     <text className="navbar-brand d-inline-block align-top mr-5 response">{props.response}</text>
